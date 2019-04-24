f69a7ec8f466bbe5c2e8791b3b5ddc15f4f958af
David Acosta <davidacosta25081@yahoo.com>
2019-04-24 00:34:01 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,13 +1,19 @@
 class Show < ActiveRecord::Base
 
-def self.highest_rating
-  self.maximum(:rating)
-end
+  def self.highest_rating
+    self.maximum(:rating)
+  end
 
-def self.most_popular_show
-      self.where("rating = ?", self.highest_rating).first
-    end 
 
+  def self.most_popular_show
+    self.where("rating = ?", self.highest_rating).first
+  end
+
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+    
 
 
 
