f97d31d4181245d652dbb7109e2ae5e52646b6ec
David Acosta <davidacosta25081@yahoo.com>
2019-04-24 00:37:09 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -13,7 +13,12 @@ class Show < ActiveRecord::Base
   def self.lowest_rating
     self.minimum(:rating)
   end
-    
+
+
+  def self.least_popular_show
+    self.where("rating = ?", self.lowest_rating).first
+  end
+      
 
 
 
